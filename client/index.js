f6354a8bbf3d41dc0471e4c97b09e18edc6151d1
JSimple <josephinemayasimple@gmail.com>
2021-11-10 15:24:13 -0500

    added: comment to test git merge

--- client/index.js
+++ client/index.js
@@ -5,7 +5,7 @@ import {Router} from 'react-router-dom'
 import history from './history'
 import store from './store'
 import App from './App'
-
+//THIS IS A TEST PLEASE REMOVE THIS COMMENT
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
