7cb7a4e6eeb8b0fba71d9ff48f0a048e45357403
Kah Yan Lee <kahyanlee6@gmail.com>
2021-11-13 15:05:24 -0600

    feat(style): styles cart to include Abstract Konnection toolbar

--- client/components/CartComponent.js
+++ client/components/CartComponent.js
@@ -2,6 +2,11 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addCartItems, removeCartItems } from '../store/cart';
 import { Link } from 'react-router-dom';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 export default function Cart(props) {
   const id = props.match.params.id;
@@ -25,72 +30,79 @@ export default function Cart(props) {
     props.history.push('/login?redirect=checkout');
   };
   return (
-    <div>
-      <div>
-        <h1>Your Cart</h1>
-        {cartItems.length === 0 ? (
-          <div>
-            <h3>Your cart is currently empty.</h3>
-            <Link to="/">Go to Products</Link>
-          </div>
-        ) : (
-          <ul>
-            {cartItems.map((item) => (
-              <li key={item.product}>
-                <div>
-                  <div>
-                    <img src={item.imageURL} alt={item.title}></img>
-                  </div>
-                  <div>
-                    <Link to={`/products/${item.product}`}>{item.title}</Link>
-                  </div>
-                  <div>
-                    <select
-                      value={item.qty}
-                      onChange={(e) =>
-                        dispatch(
-                          addCartItems(item.product, Number(e.target.value))
-                        )
-                      }
-                    >
-                      {[...Array(item.quantity).keys()].map((e) => (
-                        <option key={e + 1} value={e + 1}>
-                          {e + 1}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                  <div>Price: ${item.price}</div>
-                  <button onClick={() => removeFromCart(item.product)}>
-                    Remove From Cart
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
+    <ThemeProvider theme={theme}>
       <div>
         <div>
-          <ul>
-            <li>
-              <h2>
-                Subtotal ({cartItems.reduce((a, c) => a + Number(c.qty), 0)}{' '}
-                items) : ${cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
-              </h2>
-            </li>
-            <li>
-              <button
-                type="button"
-                onClick={checkOutItems}
-                disabled={cartItems.length === 0}
-              >
-                Proceed to Checkout
-              </button>
-            </li>
-          </ul>
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              Abstract Konnection
+            </Typography>
+          </Toolbar>
+          <h1>Your Cart</h1>
+          {cartItems.length === 0 ? (
+            <div>
+              <h3>Your cart is currently empty.</h3>
+              <Link to="/">Go to Products</Link>
+            </div>
+          ) : (
+            <ul>
+              {cartItems.map((item) => (
+                <li key={item.product}>
+                  <div>
+                    <div>
+                      <img src={item.imageURL} alt={item.title}></img>
+                    </div>
+                    <div>
+                      <Link to={`/products/${item.product}`}>{item.title}</Link>
+                    </div>
+                    <div>
+                      <select
+                        value={item.qty}
+                        onChange={(e) =>
+                          dispatch(
+                            addCartItems(item.product, Number(e.target.value))
+                          )
+                        }
+                      >
+                        {[...Array(item.quantity).keys()].map((e) => (
+                          <option key={e + 1} value={e + 1}>
+                            {e + 1}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                    <div>Price: ${item.price}</div>
+                    <button onClick={() => removeFromCart(item.product)}>
+                      Remove From Cart
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div>
+          <div>
+            <ul>
+              <li>
+                <h2>
+                  Subtotal ({cartItems.reduce((a, c) => a + Number(c.qty), 0)}{' '}
+                  items) : ${cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+                </h2>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  onClick={checkOutItems}
+                  disabled={cartItems.length === 0}
+                >
+                  Proceed to Checkout
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
