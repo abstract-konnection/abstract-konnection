20a1be69a5cd1138b0e7595b54e8ed168c649cd9
Kah Yan Lee <kahyanlee6@gmail.com>
2021-11-13 11:01:13 -0600

    fix: fixed bug in Navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -4,87 +4,81 @@ import { Link } from 'react-router-dom';
 import { logout } from '../store';
 
 const Navbar = ({ handleClick, isLoggedIn }) => {
-	let cartItem = useSelector((state) => state.cartItem);
-	const { cartItems } = cartItem;
-	return (
-		<div>
-			<h1>FS-App-Template</h1>
-			<nav>
-				{isLoggedIn ? (
-					<div>
-						{/* The navbar will show these links after you log in */}
-						<Link to="/home">Home</Link>
-						<a href="#" onClick={handleClick}>
-							Logout
-						</a>
-						<Link to="/cart">
-							Cart
-							{cartItems.length > 1 ? (
-								<div>
-									<span> {cartItems.length} Paintings In Cart</span>
-								</div>
-							) : cartItems.length === 0 ? (
-								<div>
-									<span>No Paintings In Cart</span>
-								</div>
-							) : (
-								cartItems.length ===
-								1(
-									<div>
-										<span>1 Painting In Cart</span>
-									</div>
-								)
-							)}
-						</Link>
-					</div>
-				) : (
-					<div>
-						{/* The navbar will show these links before you log in */}
-						<Link to="/">Home</Link>
-						<Link to="/cart">
-							Cart
-							{cartItems.length > 1 ? (
-								<div>
-									<span> {cartItems.length} Paintings In Cart</span>
-								</div>
-							) : cartItems.length === 0 ? (
-								<div>
-									<span>No Paintings In Cart</span>
-								</div>
-							) : (
-								cartItems.length ===
-								1(
-									<div>
-										<span>1 Painting In Cart</span>
-									</div>
-								)
-							)}
-						</Link>
-						<Link to="/login">Login</Link>
-						<Link to="/signup">Sign Up</Link>
-					</div>
-				)}
-			</nav>
-			<hr />
-		</div>
-	);
+  let cartItem = useSelector((state) => state.cartItem);
+  const { cartItems } = cartItem;
+  return (
+    <div>
+      <h1>FS-App-Template</h1>
+      <nav>
+        {isLoggedIn ? (
+          <div>
+            {/* The navbar will show these links after you log in */}
+            <Link to="/">Home</Link>
+            <a href="#" onClick={handleClick}>
+              Logout
+            </a>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 1 ? (
+                <div>
+                  <span> {cartItems.length} Paintings In Cart</span>
+                </div>
+              ) : cartItems.length === 0 ? (
+                <div>
+                  <span>No Paintings In Cart</span>
+                </div>
+              ) : (
+                <div>
+                  <span>1 Painting In Cart</span>
+                </div>
+              )}
+            </Link>
+          </div>
+        ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Link to="/">Home</Link>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 1 ? (
+                <div>
+                  <span> {cartItems.length} Paintings In Cart</span>
+                </div>
+              ) : cartItems.length === 0 ? (
+                <div>
+                  <span>No Paintings In Cart</span>
+                </div>
+              ) : (
+                <div>
+                  <span>1 Painting In Cart</span>
+                </div>
+              )}
+            </Link>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+        )}
+      </nav>
+      <hr />
+    </div>
+  );
 };
 
 /**
  * CONTAINER
  */
 const mapState = (state) => {
-	return {
-		isLoggedIn: !!state.auth.id,
-	};
+  return {
+    isLoggedIn: !!state.auth.id,
+  };
 };
 
 const mapDispatch = (dispatch) => {
-	return {
-		handleClick() {
-			dispatch(logout());
-		},
-	};
+  return {
+    handleClick() {
+      dispatch(logout());
+    },
+  };
 };
 
 export default connect(mapState, mapDispatch)(Navbar);
