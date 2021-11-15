f7c8f0f9bfba1e0f6c17ed97c3da5f601e83b007
Daryl Ramirez <darylc.ramirez@gmail.com>
2021-11-15 12:25:00 -0500

    (fix): Admin now has cart view

--- client/Routes.js
+++ client/Routes.js
@@ -18,73 +18,74 @@ import NotFound from './components/NotFound';
  * COMPONENT
  */
 class Routes extends Component {
-  componentDidMount() {
-    this.props.loadInitialData();
-  }
+	componentDidMount() {
+		this.props.loadInitialData();
+	}
 
-  render() {
-    const { isLoggedIn } = this.props;
-    const { isAdmin } = this.props;
+	render() {
+		const { isLoggedIn } = this.props;
+		const { isAdmin } = this.props;
 
-    return (
-      <div>
-        {isLoggedIn && isAdmin ? (
-          <Switch>
-            <Route exact path="/admin" component={AdminMainView} />
-            <Route exact path="/admin/products" component={AdminAllProducts} />
-            <Route exact path="/admin/users" component={AdminViewUsers} />
-            <Route
-              exact
-              path="/admin/products/:id"
-              component={AdminSingleProduct}
-            />
-            <Route exact path="/" exact component={AllProducts} />
-            <Route exact path="/products/:id" component={SingleProduct} />
-            <Route exact path="/checkout" component={Checkout} />
-            <Redirect to="/" />
-          </Switch>
-        ) : isLoggedIn ? (
-          <Switch>
-            <Route exact path="/" exact component={AllProducts} />
-            <Route exact path="/products/:id" component={SingleProduct} />
-            <Route exact path="/checkout" component={Checkout} />
-            <Route exact path="/cart" component={Cart} />
-            <Redirect to="/" />
-          </Switch>
-        ) : (
-          <Switch>
-            <Route exact path="/" exact component={AllProducts} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/products/:id" component={SingleProduct} />
-            <Route exact path="/checkout" component={Checkout} />
-            <Route exact path="/cart" component={Cart} />
-            <Redirect to="/" />
-          </Switch>
-        )}
-      </div>
-    );
-  }
+		return (
+			<div>
+				{isLoggedIn && isAdmin ? (
+					<Switch>
+						<Route exact path="/admin" component={AdminMainView} />
+						<Route exact path="/admin/products" component={AdminAllProducts} />
+						<Route exact path="/admin/users" component={AdminViewUsers} />
+						<Route
+							exact
+							path="/admin/products/:id"
+							component={AdminSingleProduct}
+						/>
+						<Route exact path="/" exact component={AllProducts} />
+						<Route exact path="/products/:id" component={SingleProduct} />
+						<Route exact path="/cart" component={Cart} />
+						<Route exact path="/checkout" component={Checkout} />
+						<Redirect to="/" />
+					</Switch>
+				) : isLoggedIn ? (
+					<Switch>
+						<Route exact path="/" exact component={AllProducts} />
+						<Route exact path="/products/:id" component={SingleProduct} />
+						<Route exact path="/checkout" component={Checkout} />
+						<Route exact path="/cart" component={Cart} />
+						<Redirect to="/" />
+					</Switch>
+				) : (
+					<Switch>
+						<Route exact path="/" exact component={AllProducts} />
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/signup" component={Signup} />
+						<Route exact path="/products/:id" component={SingleProduct} />
+						<Route exact path="/checkout" component={Checkout} />
+						<Route exact path="/cart" component={Cart} />
+						<Redirect to="/" />
+					</Switch>
+				)}
+			</div>
+		);
+	}
 }
 
 /**
  * CONTAINER
  */
 const mapState = (state) => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
-    // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-    isLoggedIn: !!state.auth.id,
-    isAdmin: !!state.auth.admin,
-  };
+	return {
+		// Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
+		// Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
+		isLoggedIn: !!state.auth.id,
+		isAdmin: !!state.auth.admin,
+	};
 };
 
 const mapDispatch = (dispatch) => {
-  return {
-    loadInitialData() {
-      dispatch(me());
-    },
-  };
+	return {
+		loadInitialData() {
+			dispatch(me());
+		},
+	};
 };
 
 // The `withRouter` wrapper makes sure that updates are not blocked
