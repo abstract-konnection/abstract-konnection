aca7ae89403bdf16d9c3a61a61c0efe660f2656e
Kah Yan Lee <kahyanlee6@gmail.com>
2021-11-15 07:51:31 -0600

    fix: misses a merge conflict, resolved now

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -11,84 +11,78 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = ({ handleClick, isLoggedIn, isAdmin }) => (
-	<Box sx={{ flexGrow: 1 }}>
-		<AppBar position="static">
-			<Toolbar>
-				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-					<Link to="/" style={{ color: '#FFF' }}>
-						AK(c)
-					</Link>
-				</Typography>
-				{isLoggedIn && isAdmin ? (
-					<div>
-						<Link to="/admin" style={{ color: '#FFF' }}>
-							<Button color="inherit">Admin</Button>
-						</Link>
-						<Link to="/" style={{ color: '#FFF' }}>
-							<Button color="inherit">Home</Button>
-						</Link>
-						<Link to="/cart" style={{ color: '#FFF' }}>
-							<Button color="inherit">Cart</Button>
-						</Link>
-						<Button color="inherit" onClick={handleClick}>
-							Logout
-						</Button>
-					</div>
-				) : isLoggedIn ? (
-					<div>
-						<Link to="/" style={{ color: '#FFF' }}>
-							<Button color="inherit">Home</Button>
-						</Link>
-						<Link to="/cart" style={{ color: '#FFF' }}>
-							<Button color="inherit">Cart</Button>
-						</Link>
-						<Button color="inherit" onClick={handleClick}>
-							Logout
-						</Button>
-					</div>
-				) : (
-					<div>
-						<Link to="/">
-							<Button color="inherit" style={{ color: '#FFF' }}>
-								Home
-							</Button>
-						</Link>
-						<Link to="/login">
-							<Button color="inherit" style={{ color: '#FFF' }}>
-								Login
-							</Button>
-						</Link>
-						<Link to="/signup">
-							<Button color="inherit" style={{ color: '#FFF' }}>
-								Sign Up
-							</Button>
-						</Link>
-						<Link to="/cart">
-							<Button color="inherit" style={{ color: '#FFF' }}>
-								Cart
-							</Button>
-						</Link>
-					</div>
-				)}
-			</Toolbar>
-		</AppBar>
-	</Box>
+  <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Link to="/" style={{ color: '#FFF' }}>
+            AK(c)
+          </Link>
+        </Typography>
+        {isLoggedIn && isAdmin ? (
+          <div>
+            <Link to="/admin" style={{ color: '#FFF' }}>
+              <Button color="inherit">Admin</Button>
+            </Link>
+            <Link to="/" style={{ color: '#FFF' }}>
+              <Button color="inherit">Home</Button>
+            </Link>
+            <Link to="/cart" style={{ color: '#FFF' }}>
+              <Button color="inherit">Cart</Button>
+            </Link>
+            <Button color="inherit" onClick={handleClick}>
+              Logout
+            </Button>
+          </div>
+        ) : isLoggedIn ? (
+          <div>
+            <Link to="/" style={{ color: '#FFF' }}>
+              <Button color="inherit">Home</Button>
+            </Link>
+            <Link to="/cart" style={{ color: '#FFF' }}>
+              <Button color="inherit">Cart</Button>
+            </Link>
+            <Button color="inherit" onClick={handleClick}>
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <div>
+            <Link to="/">
+              <Button color="inherit" style={{ color: '#FFF' }}>
+                Home
+              </Button>
+            </Link>
+            <Link to="/login">
+              <Button color="inherit" style={{ color: '#FFF' }}>
+                Login
+              </Button>
+            </Link>
+            <Link to="/signup">
+              <Button color="inherit" style={{ color: '#FFF' }}>
+                Sign Up
+              </Button>
+            </Link>
+            <Link to="/cart">
+              <Button color="inherit" style={{ color: '#FFF' }}>
+                Cart
+              </Button>
+            </Link>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
+  </Box>
 );
 
 /**
  * CONTAINER
  */
 const mapState = (state) => {
-<<<<<<< HEAD
   return {
     isLoggedIn: !!state.auth.id,
+    isAdmin: !!state.auth.admin,
   };
-=======
-	return {
-		isLoggedIn: !!state.auth.id,
-		isAdmin: !!state.auth.admin,
-	};
->>>>>>> 7907b81abb2450d05da0a15b9a25257c13a07c0c
 };
 
 const mapDispatch = (dispatch) => {
