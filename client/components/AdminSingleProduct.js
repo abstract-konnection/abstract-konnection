778273f3c3f010f968d08a2cc179d1db0c786b42
Daryl Ramirez <darylc.ramirez@gmail.com>
2021-11-17 12:29:03 -0500

    loading spinner for admin view

--- client/components/AdminSingleProduct.js
+++ client/components/AdminSingleProduct.js
@@ -4,6 +4,7 @@ import { setProducts } from '../store/product';
 import { styled } from '@mui/material/styles';
 import { Paper, Grid, Button, Typography } from '@mui/material';
 import AdminUpdateProduct from './AdminUpdateProduct';
+import { CircularProgress } from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
 	...theme.typography.body,
@@ -16,12 +17,19 @@ const Item = styled(Paper)(({ theme }) => ({
 class AdminSingleProduct extends Component {
 	constructor() {
 		super();
+		this.state = {
+			isLoading: false,
+		};
 	}
+
 	async componentDidMount() {
+		this.setState({ isLoading: true });
 		try {
 			const id = await this.props.match.params.id;
 			await this.props.loadSingleProduct(id);
+			this.setState({ isLoading: false });
 		} catch (error) {
+			this.setState({ isLoading: false });
 			console.log('Single Product', error);
 		}
 	}
@@ -29,27 +37,47 @@ class AdminSingleProduct extends Component {
 	render() {
 		const product = this.props.product || {};
 		return (
-			<Grid container spacing={0} alignItems="center" justifyContent="center">
-				<div>
-					<main>
-						<Typography
-							component="h1"
-							variant="h3"
-							align="center"
-							color="text.primary"
-							gutterBottom>
-							Administrator View
-						</Typography>
-						<Item>
-							<img src={product.imageURL} />
-							<h1>{product.title}</h1>
-							<p>{product.description}</p>
-							<h3>${product.price}</h3>
-						</Item>
-						<AdminUpdateProduct />
-					</main>
-				</div>
-			</Grid>
+			<div>
+				{this.state.isLoading === true ? (
+					<div>
+						<main>
+							<Grid
+								container
+								spacing={0}
+								alignItems="center"
+								justifyContent="center">
+								<CircularProgress color="secondary" />
+							</Grid>
+						</main>
+					</div>
+				) : (
+					<Grid
+						container
+						spacing={0}
+						alignItems="center"
+						justifyContent="center">
+						<div>
+							<main>
+								<Typography
+									component="h1"
+									variant="h3"
+									align="center"
+									color="text.primary"
+									gutterBottom>
+									Administrator View
+								</Typography>
+								<Item>
+									<img src={product.imageURL} />
+									<h1>{product.title}</h1>
+									<p>{product.description}</p>
+									<h3>${product.price}</h3>
+								</Item>
+								<AdminUpdateProduct />
+							</main>
+						</div>
+					</Grid>
+				)}
+			</div>
 		);
 	}
 }
